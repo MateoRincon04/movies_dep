b798c700c75ce61891d7e36f9632903c3bffcd8b
Mateo.Rincon04 <mateo.rincona@perficient.com>
2022-05-09 11:14:44 -0500

    trying to make dynamic inv work

--- IaC/modules/scripts/frontend.sh
+++ IaC/modules/scripts/frontend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "PROVISIONING --------------------------------"
-ssh ${BASTION_HOST} -- ansible-playbook -i ~/movie_dep/IaC/Ansible/aws_ec2_inventory.yml -e "tag=\"0.0.1\" BACK_HOST=${BACK_HOST}" --private-key /home/ubuntu/.ssh/id_rsa -u ubuntu ../../Ansible/frontend.yml
+ssh ${BASTION_HOST} -- ansible-playbook -i ~/movie_dep/IaC/Ansible/aws_ec2_inventory.yml -e "tag=\"0.0.1\" BACK_HOST=${BACK_HOST}" --private-key /home/ubuntu/.ssh/id_rsa -u ubuntu ~/movies_dep/Iac/Ansible/frontend.yml
 echo "-------------------------------------"
