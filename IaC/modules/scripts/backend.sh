773dcdbeec82d397490ed091fcfed81e02157aa0
Mateo.Rincon04 <mateo.rincona@perficient.com>
2022-05-11 14:32:54 -0500

    working on backend script

--- IaC/modules/scripts/backend.sh
+++ IaC/modules/scripts/backend.sh
@@ -3,7 +3,5 @@
 # scp ubuntu@$BASTION_HOST:~/movies_dep/IaC/Ansible ~/Ansible
 
 echo "PROVISIONING --------------------------------"
-echo "${BASTION_HOST}"
-echo "---------------------------------------------"
 ssh ${BASTION_HOST} -- ansible-playbook -i ~/movie_dep/IaC/Ansible/aws_ec2.yml -e "DB_PASS=${DB_PASS} DB_HOST=${DB_HOST}" --private-key /home/ubuntu/.ssh/id_rsa -u ubuntu ~/movies_dep/IaC/Ansible/backend.yml
 echo "-------------------------------"
